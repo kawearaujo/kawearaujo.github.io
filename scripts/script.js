7d5649ad63fb82745256e24bcfb57257380d135b
kawearaujo <kawe.apr@gmail.com>
2022-04-13 20:24:07 -0300

    Front-End finalizado

--- scripts/script.js
+++ scripts/script.js
@@ -4,8 +4,29 @@ $(document).ready(function(){
             $('.navbar').addClass("sticky");
         }else{
             $('.navbar').removeClass("sticky");
+        }if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
         }
     });
+        $('.scroll-up-btn').click(function(){
+            $('html').animate({scrollTop:0});
+        });
+
+        var typed = new Typed(".typing", {
+            strings:["Dev Front-End","Game Design","Dev Back-End","Freelancer"],
+            typeSpeed:100,
+            backSpeed:60,
+            loop:true
+        });
+        var typed = new Typed(".typing-2", {
+            strings:["Dev Front-End","Game Design","Dev Back-End","Freelancer"],
+            typeSpeed:100,
+            backSpeed:60,
+            loop:true
+        });
+
     $('.menu-btn').click(function () {
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");       
