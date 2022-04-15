b7a416c18dd06000b60c81fc9f895bab8c0a2019
kawearaujo <kawe.apr@gmail.com>
2022-04-15 15:59:30 -0300

    novos: prototipos, cores..

---
+++ scripts/form.php
@@ -0,0 +1,26 @@
+<?
+
+$destinatario = "kawe.apr@gmail.com";
+
+$nome = $_REQUEST['nome'];
+$email = $_REQUEST['email'];
+$assunto = $_REQUEST['assunto'];
+$mensagem = $_REQUEST['mensagem'];
+
+
+$body = "===================================" . "\n";
+$body = $body . "FALE CONOSCO - TESTE COMPROVATIVO" . "\n";
+$body = $body . "===================================" . "\n\n";
+$body = $body . "Nome: " . $nome . "\n";
+$body = $body . "Email: " . $email . "\n";
+$body = $body . "Mensagem: " . $mensagem . "\n\n";
+$body = $body . "===================================" . "\n";
+
+// envia o email
+mail($destinatario, $assunto , $body, "From: $email\r\n");
+
+// redireciona para a página de obrigado
+header("location:index.html");
+
+
+?>
